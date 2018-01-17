67ea16bc9a137f194e6b0686658e951bb290a2eb
antoinebartholomew <antoinebartholomew@gmail.com>
2018-01-17 18:04:19 -0500

    2nd commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,92 @@
-// create variables for game
-var position = 0;
-var correctAnswer = 0;
-var trivia;
-var trivia_status;
-var question;
-var userSelectedChoice;
-var choices;
-var choiceA;
-var choiceB;
-var choiceC;
-// create a multimentional array of ..questions [index:0] along with the Choice A[index:1], Choice B [index:2], Choice C [index:3] and answer [index:4] for game
-var questions =[
-  ["What two jersey numbers did Kobe Bryant wear?" , "8 & 24", "10 & 30", "34 & 23", "A" ],
-  ["What number did Micheal Jordan wear?" , "37", "23", "45", "B"],
-  ["Who is the NBA all time scoring leader?", "Kobe Bryant", "Micheal Jordan", "Karrem Abdual Jabar", "C"],
-  ["Who won more MVP awards?", "Steve Nash", "Kobe Bryant", "Allen Iverson", "A"  ],
-  ["What year did LeBron James win his first NBA title in Cleveland?", "2015", "2013", "2011", "A"]
-];
-function triviaReturn(x){
-  return document.getElementById(x);
-};
-function formulateQuestion(){
-        trivia = triviaReturn("trivia_question");
-        if(position >= questions.length){
-          trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+ questions.length+" questions correct!</h2>";
-          triviaReturn("trivia_progress").innerHTML= "Final Score!";
-          position = 0;
-          correctAnswer = 0;
-          return false;
-        }
-        triviaReturn("trivia_progress").innerHTML = "Question " +(position+1)+" of "+questions.length;
-        question = questions[position][0];
-        choiceA = questions[position][1];
-        choiceB = questions[position][2];
-        choiceC = questions[position][3];
-        trivia_question.innerHTML = "<h3>"+question+"</h3>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'A'> "+choiceA+"<br>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'B'> "+choiceB+"<br>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'C'> "+choiceC+"<br>";
-        trivia_question.innerHTML += "<button onclick= 'checkAnswer()'> Submit Answer </button>";
-        if(position >= question.length){
-          trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+question.length+"question correct!</h2>"}
-};
-function checkAnswer(){
-  choices = document.getElementsByName('choices');
-  for (var i = 0; i < choices.length; i++) {
-    if (choices[i].checked) {
-      choice = choices[i].value;
-    }
-  }
-  if (choice == questions[position][4]){
-    correctAnswer++;
-  }
-  position++;
-  formulateQuestion();
+
+// 1. Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+  authDomain: "time-sheet-55009.firebaseapp.com",
+  databaseURL: "https://time-sheet-55009.firebaseio.com",
+  storageBucket: "time-sheet-55009.appspot.com"
 };
 
+firebase.initializeApp(config);
 
-//https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward?rq=1
-var timeLeft = 30;
-var elem = document.getElementById('trivia_timer');
-var timerId = setInterval(countdown, 1000);
-function countdown() {
-  if (timeLeft == 0) {
-    trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+ questions.length+" questions correct!</h2>";
-      triviaReturn("trivia_progress").innerHTML= "Final Score!";
-      position = 0;
-      correctAnswer = 0;
-      return false;
-  } else {
-    elem.innerHTML = timeLeft + ' seconds remaining';
-    timeLeft--;
-    //alert("Hello! I am an alert box!!");
-  }
-};
+var database = firebase.database();
 
+// 2. Button for adding Employees
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
 
+  // Grabs user input
+  var trainName = $("#train-name").val().trim();
+  var Destination = $("#Destination").val().trim();
+  // change line 31 to military time
+  var FirstTrainTime = moment($("#FirstTrainTime").val().trim(), "DD/MM/YY").format("X");
+  var Frequency = $("#Frequency").val().trim();
 
-window.addEventListener("load", formulateQuestion, false)
+  // Creates local "temporary" object for holding employee data
+  var newTrain = {
+    Name: trainName,
+    whereTo: Destination,
+    TrainTime: FirstTrainTime,
+    Frequency1: Frequency
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(trainName.name);
+  console.log(Destination.whereTo);
+  console.log(FirstTrainTime.TrainTime);
+  console.log(Frequency.Frequency1);
+
+  // Alert
+  alert("New Train Time successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name").val("");
+  $("#Destination").val("");
+  $("#FirstTrainTime").val("");
+  $("#Frequency").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().Name;
+  var Destination = childSnapshot.val().whereTo;
+  var FirstTrainTime = childSnapshot.val().TrainTime;
+  var Frequency = childSnapshot.val().Frequency1;
+
+  // Employee Info
+  console.log(trainName);
+  console.log(Destination);
+  console.log(FirstTrainTime);
+  console.log(Frequency);
+
+  // Prettify the employee start
+  //var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  //var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+  //console.log(empMonths);
+
+  // Calculate the total billed rate
+  //var empBilled = empMonths * empRate;
+  //console.log(empBilled);
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + Destination + "</td><td>" +
+  FirstTrainTime + "</td><td>" + Frequency + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+});
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume Employee start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any attempt we use mets this test case
